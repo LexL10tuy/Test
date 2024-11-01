7dd6095d378df3e3139add082e9297d74b94ffee
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-11-01 18:40:37 +0000

    Сделал 3 задачу

---
+++ cikle.py
@@ -0,0 +1,15 @@
+answer = " "
+while answer != "Да":
+    print('Купи велосипед')
+    answer = input('Купил? Да/Нет ')
+print("Проколи колесо")
+result = ' '
+while result != 'Да':
+    print('Проколи колесо')
+    result = input('Проколол? Да/Нет ')
+print('Иди в Спортмастер')
+find = ' '
+while find != 'Да':
+    print('Найди продавца')
+    find = input('Нашел продавца? Да/Нет ')
+print('Попроси его принести подходящую шину')
