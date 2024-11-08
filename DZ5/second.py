6c3d31796331c2aa175cb392b0b4ac5e13ec2ae2
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-11-08 17:06:00 +0000

    Did it

---
+++ DZ5/second.py
@@ -0,0 +1,29 @@
+month = int(input('Введите номер месяца: '))
+match month:
+    case 1:
+        print('Январь')
+    case 2:
+        print('Февраль')
+    case 3:
+        print('Март')
+    case 4:
+        print('Апрель')
+    case 5:
+        print('Май')
+    case 6:
+        print('Июнь')
+    case 7:
+        print('Июль')
+    case 8:
+        print('Август')
+    case 9:
+        print('Сентябрь')
+    case 10:
+        print('Октябрь')
+    case 11:
+        print('Ноябрь')
+    case 12:
+        print('Декабрь')
+    case _:
+        print('Нет такого месяца')
+    
