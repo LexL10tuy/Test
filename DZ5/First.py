777331a698fcc3fb18b3f86331cbd555cb4525ad
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-11-08 16:43:15 +0000

    Did it

---
+++ DZ5/First.py
@@ -0,0 +1,17 @@
+day = int(input('Введите день недели '))
+if day == 1:
+    print('Monday')
+elif day == 2:
+    print('Thuesday')
+elif day == 3:
+    print('Wendsday')
+elif day == 4:
+    print('Thurtuday')
+elif day == 5:
+    print('Friday')
+elif day == 6:
+    print('Sunday')
+elif day == 7:
+    print('Saturday')
+else:
+    print('Нет такого дня недели')
