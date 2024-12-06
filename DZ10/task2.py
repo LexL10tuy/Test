ed892c10688be79ba5050cdefb7cdbd9c4a68653
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-12-06 17:23:14 +0000

    Eeeee

---
+++ DZ10/task2.py
@@ -0,0 +1,9 @@
+def multiplication_table():
+    print("Таблица умножения от 1 до 10: ")
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(f'{i} * {j} = {i * j}')
+            print()
+
+
+multiplication_table()
