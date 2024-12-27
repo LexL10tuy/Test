b29563a5461b1c663d5ff5fd3533ecacb8cf32ff
LexL10tuy <aleshinalesha19011991@gmail.com>
2024-12-27 18:28:51 +0000

    frol

--- task20.py
+++ task20.py
@@ -2,6 +2,6 @@ num = input("Введите четырехзначное число: ")
 print(f"{num[3] + num[2] + num[1] + num[0]}")
 
 print(f"{int(num[2] + num[3] + num[0] + num[1])}")
-num_pro1 = int(num) // 100
-num_pro2 = int(num) % 100
-print(f'{str(num_pro2) + str(num_pro1)}')
+num_pro1 = str(int(num) // 100)
+num_pro2 = str(int(num) % 100)
+print(num_pro2 + num_pro1)
---
+++ task24.py
@@ -0,0 +1,10 @@
+a = '237'
+a = a.replace('2', ' ')
+
+
+a = int(a) * 10
+
+
+a = str(a)
+a = a.replace('0', '2')
+print(a)
